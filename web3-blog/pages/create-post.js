f37cb0ea43c1d9a1f0704dad21d4effee4e1e3d0
Aleksander Wojcik <aleksander.w1992@gmail.com>
2022-07-04 14:39:04 +0200

    fix with copy-pasting .js script

--- web3-blog/pages/create-post.js
+++ web3-blog/pages/create-post.js
@@ -1,144 +1,185 @@
-/* pages/index.js */
-import { css } from '@emotion/css'
-import { useContext } from 'react'
+/* pages/create-post.js */
+import { useState, useRef, useEffect } from 'react' // new
 import { useRouter } from 'next/router'
+import dynamic from 'next/dynamic'
+import { css } from '@emotion/css'
 import { ethers } from 'ethers'
-import Link from 'next/link'
-import { AccountContext } from '../context'
+import { create } from 'ipfs-http-client'
 
 /* import contract address and contract owner address */
 import {
-  contractAddress, ownerAddress
+  contractAddress
 } from '../config'
 
-/* import Application Binary Interface (ABI) */
 import Blog from '../artifacts/contracts/Blog.sol/Blog.json'
 
-export default function Home(props) {
-  /* posts are fetched server side and passed in as props */
-  /* see getServerSideProps */
-  const { posts } = props
-  const account = useContext(AccountContext)
+/* define the ipfs endpoint */
+const client = create('https://ipfs.infura.io:5001/api/v0')
 
+/* configure the markdown editor to be client-side import */
+const SimpleMDE = dynamic(
+    () => import('react-simplemde-editor'),
+    { ssr: false }
+)
+
+const initialState = { title: '', content: '' }
+
+function CreatePost() {
+  /* configure initial state to be used in the component */
+  const [post, setPost] = useState(initialState)
+  const [image, setImage] = useState(null)
+  const [loaded, setLoaded] = useState(false)
+
+  const fileRef = useRef(null)
+  const { title, content } = post
   const router = useRouter()
-  async function navigate() {
-    router.push('/create-post')
+
+  useEffect(() => {
+    setTimeout(() => {
+      /* delay rendering buttons until dynamic import is complete */
+      setLoaded(true)
+    }, 500)
+  }, [])
+
+  function onChange(e) {
+    setPost(() => ({ ...post, [e.target.name]: e.target.value }))
+  }
+
+  async function createNewPost() {
+    /* saves post to ipfs then anchors to smart contract */
+    if (!title || !content) return
+    const hash = await savePostToIpfs()
+    await savePost(hash)
+    router.push(`/`)
+  }
+
+  async function savePostToIpfs() {
+    /* save post metadata to ipfs */
+    try {
+      const added = await client.add(JSON.stringify(post))
+      return added.path
+    } catch (err) {
+      console.log('error: ', err)
+    }
+  }
+
+  async function savePost(hash) {
+    /* anchor post to smart contract */
+    if (typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract(contractAddress, Blog.abi, signer)
+      console.log('contract: ', contract)
+      try {
+        const val = await contract.createPost(post.title, hash)
+        /* optional - wait for transaction to be confirmed before rerouting */
+        /* await provider.waitForTransaction(val.hash) */
+        console.log('val: ', val)
+      } catch (err) {
+        console.log('Error: ', err)
+      }
+    }
+  }
+
+  function triggerOnChange() {
+    /* trigger handleFileChange handler of hidden file input */
+    fileRef.current.click()
+  }
+
+  async function handleFileChange (e) {
+    /* upload cover image to ipfs and save hash to state */
+    const uploadedFile = e.target.files[0]
+    if (!uploadedFile) return
+    const added = await client.add(uploadedFile)
+    setPost(state => ({ ...state, coverImage: added.path }))
+    setImage(uploadedFile)
   }
 
   return (
-      <div>
-        <div className={postList}>
-          {
-            /* map over the posts array and render a button with the post title */
-            posts.map((post, index) => (
-                <Link href={`/post/${post[2]}`} key={index}>
-                  <a>
-                    <div className={linkStyle}>
-                      <p className={postTitle}>{post[1]}</p>
-                      <div className={arrowContainer}>
-                        <img
-                            src='/right-arrow.svg'
-                            alt='Right arrow'
-                            className={smallArrow}
-                        />
-                      </div>
-                    </div>
-                  </a>
-                </Link>
-            ))
-          }
-        </div>
-        <div className={container}>
-          {
-            (account === ownerAddress) && posts && !posts.length && (
-                /* if the signed in user is the account owner, render a button */
-                /* to create the first post */
-                <button className={buttonStyle} onClick={navigate}>
-                  Create your first post
-                  <img
-                      src='/right-arrow.svg'
-                      alt='Right arrow'
-                      className={arrow}
-                  />
-                </button>
-            )
-          }
-        </div>
+      <div className={container}>
+        {
+          image && (
+              <img className={coverImageStyle} src={URL.createObjectURL(image)} />
+          )
+        }
+        <input
+            onChange={onChange}
+            name='title'
+            placeholder='Give it a title ...'
+            value={post.title}
+            className={titleStyle}
+        />
+        <SimpleMDE
+            className={mdEditor}
+            placeholder="What's on your mind?"
+            value={post.content}
+            onChange={value => setPost({ ...post, content: value })}
+        />
+        {
+          loaded && (
+              <>
+                <button
+                    className={button}
+                    type='button'
+                    onClick={createNewPost}
+                >Publish</button>
+                <button
+                    onClick={triggerOnChange}
+                    className={button}
+                >Add cover image</button>
+              </>
+          )
+        }
+        <input
+            id='selectImage'
+            className={hiddenInput}
+            type='file'
+            onChange={handleFileChange}
+            ref={fileRef}
+        />
       </div>
   )
 }
 
-export async function getServerSideProps() {
-  /* here we check to see the current environment variable */
-  /* and render a provider based on the environment we're in */
-  let provider
-  if (process.env.ENVIRONMENT === 'local') {
-    provider = new ethers.providers.JsonRpcProvider()
-  } else if (process.env.ENVIRONMENT === 'testnet') {
-    provider = new ethers.providers.JsonRpcProvider('https://rpc-mumbai.matic.today')
-  } else {
-    provider = new ethers.providers.JsonRpcProvider('https://polygon-rpc.com/')
+const hiddenInput = css`
+  display: none;
+`
+
+const coverImageStyle = css`
+  max-width: 800px;
+`
+
+const mdEditor = css`
+  margin-top: 40px;
+`
+
+const titleStyle = css`
+  margin-top: 40px;
+  border: none;
+  outline: none;
+  background-color: inherit;
+  font-size: 44px;
+  font-weight: 600;
+  &::placeholder {
+    color: #999999;
   }
-
-  const contract = new ethers.Contract(contractAddress, Blog.abi, provider)
-  const data = await contract.fetchPosts()
-  return {
-    props: {
-      posts: JSON.parse(JSON.stringify(data))
-    }
-  }
-}
-
-const arrowContainer = css`
-  display: flex;
-  flex: 1;
-  justify-content: flex-end;
-  padding-right: 20px;
-`
-
-const postTitle = css`
-  font-size: 30px;
-  font-weight: bold;
-  cursor: pointer;
-  margin: 0;
-  padding: 20px;
-`
-
-const linkStyle = css`
-  border: 1px solid #ddd;
-  margin-top: 20px;
-  border-radius: 8px;
-  display: flex;
-`
-
-const postList = css`
-  width: 700px;
-  margin: 0 auto;
-  padding-top: 50px;  
 `
 
 const container = css`
-  display: flex;
-  justify-content: center;
+  width: 800px;
+  margin: 0 auto;
 `
 
-const buttonStyle = css`
-  margin-top: 100px;
+const button = css`
   background-color: #fafafa;
   outline: none;
   border: none;
-  font-size: 44px;
-  padding: 20px 70px;
   border-radius: 15px;
   cursor: pointer;
+  margin-right: 10px;
+  font-size: 18px;
+  padding: 16px 70px;
   box-shadow: 7px 7px rgba(0, 0, 0, .1);
 `
 
-const arrow = css`
-  width: 35px;
-  margin-left: 30px;
-`
-
-const smallArrow = css`
-  width: 25px;
-`
+export default CreatePost
